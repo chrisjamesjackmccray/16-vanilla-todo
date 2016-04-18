a62adb12f0b51cb421be9b6aa150c6dea10dbc80
James McCray <chrisjamesjackmccray@gmail.com>
2016-04-18 16:08:03 -0500

    Assignment Commit

--- app/app.js
+++ app/app.js
@@ -1,21 +1,54 @@
-document 
+let todos = [];
+
+document
   .querySelector("#button")
   .addEventListener("click", function(e) {
-  let textinput = document.querySelector("#textfield");
-  console.log(textinput);
+    let textinput = document.querySelector("#textfield");
+    let todo = new Todo(textinput.value);
+    todos.push(todo);
+    console.log(todos);
+    render();
   });
+
+ // class alltodos = {
+ //   let todoone = document.createElement("div");
+ //   list.innerHTML = innerHTML + 'Bag'
+ //
+ //   let todotwo = document.createElement("div");
+ //   list.innerHTML = innerHTML + 'Orange'
+ //
+ //   let todothree = document.createElement("div");
+ //   list.innerHTML = innerHTML + 'Fruit'
+ // }
 
 
 
 function Todo(item) {
   this.item = item;
   this.completed = false;
-  this.todoarray = [];
-
 }
 
-document
-  .querySelector("#textfield")
-  .addEventListener("click", function(e) {
-    this.todoarray.push(textinput);
-  });
+
+function render() {
+ document.querySelector("#todos").innerHTML = "";
+
+ todos.forEach((todo) => {
+   let div = document.createElement('div');
+   div.classList.add("todo");
+   div.innerHTML = todo.item;
+   document.querySelector("#todos").appendChild(div);
+
+   console.log(div);
+ })
+}
+
+
+
+
+
+
+
+// document
+//   .querySelector("#textfield")
+//   .addEventListener("click", function(e) {
+//   });
